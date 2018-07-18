74e2b6827d04f9344bfc83029d1977b611c789f4
Mahdan Ahmad Fauzi Al-Hasan <mahdan.hasan@un.or.id>
2018-07-18 14:06:14 +0700

    zooming down done

--- js/map.js
+++ js/map.js
@@ -3,7 +3,7 @@ let map_id		= "map-viz";
 
 let path;
 const states	= ['prov', 'kabs', 'kecs', 'vils'];
-let curr_state	= 0;
+let curr_state	= -1;
 
 let mappedGeo	= {};
 let centered	= {};
@@ -11,6 +11,7 @@ let centered	= {};
 let base_font	= 10;
 let base_stroke	= .5;
 let base_opac	= .75;
+let scale		= 1;
 
 function initMap() {
 	d3.select(map_dest).selectAll("svg").remove();
@@ -42,76 +43,97 @@ function initMap() {
 		.attr('height', height)
 		.on('click', () => zoom(null));
 
-	d3.json('json/0.json', (err, raw) => {
-		let prov		= topojson.feature(raw, raw.objects.map);
-		mappedGeo.prov	= _.chain(prov).get('features', []).keyBy('properties.id').mapValues((o) => ({ centroid: path.centroid(o), bounds: path.bounds(o) })).value();
-
-		let grouped	= svg.append('g').attr('id', 'prov-wrapper')
-			.selectAll('path.prov').data(prov.features).enter().append('g')
-				.attr('id', (o) => (o.properties.id))
-				.attr('class', 'grouped-prov cursor-pointer');
-
-		grouped.append('path')
-			.attr('d', path)
-			.attr('class', 'prov')
-			.attr('vector-effect', 'non-scaling-stroke')
-			.style('stroke-opacity', base_opac)
-			.style('stroke-width', base_stroke + 'px');
-
-		grouped.append('text')
-			.attr('x', (o) => (mappedGeo.prov[o.properties.id].centroid[0]))
-			.attr('y', (o) => (mappedGeo.prov[o.properties.id].centroid[1]))
-			.style('font-size', base_font + 'px')
-			.text((o) => (o.properties.name));
-
-		grouped.on('click', (o) => zoom(o.properties.id));
-	});
+	drawMap(0, 'prov');
 }
 
-function zoom(id) {
+function zoom(id, state) {
 	let svg	= d3.select("svg#" + map_id + '> g');
 
-	if (path && svg.node()) {
-		let x, y, k;
+	if (path && svg.node() && (state !== _.last(states))) {
+		let x, y;
 		let node	= svg.node().getBBox();
 
-		console.log(curr_state);
+		curr_state	= states.indexOf(state);
 
-		if (id && centered[states[curr_state - 1]] !== id) {
-			x = mappedGeo[states[curr_state]][id].centroid[0];
-			y = mappedGeo[states[curr_state]][id].centroid[1];
+		if (id && centered[state] !== id) {
+			x = mappedGeo[state][id].centroid[0];
+			y = mappedGeo[state][id].centroid[1];
 
-			dx = mappedGeo[states[curr_state]][id].bounds[1][0] - mappedGeo[states[curr_state]][id].bounds[0][0],
-      		dy = mappedGeo[states[curr_state]][id].bounds[1][1] - mappedGeo[states[curr_state]][id].bounds[0][1],
+			dx = mappedGeo[state][id].bounds[1][0] - mappedGeo[state][id].bounds[0][0],
+      		dy = mappedGeo[state][id].bounds[1][1] - mappedGeo[state][id].bounds[0][1],
 
-			k = Math.max(node.width * .35 / dx, node.height * .35 / dy);
+			scale = Math.max(node.width * .35 / dx, node.height * .35 / dy);
 
-			centered[states[curr_state]]	= id;
-			curr_state++;
+			if (centered[state]) {
+				svg.select('g#' + 'wrapped-' + centered[state]).remove();
+				svg.select('g#' + state + '-' + centered[state]).classed('hidden', false);
+			}
+			svg.select('g#' + state + '-' + id).classed('hidden', true);
+			centered[state]	= id;
+
+			drawMap(id, states[curr_state + 1]);
+		} else if (_.isNil(state)) {
+			x = node.width / 2;
+			y = node.height / 2;
+			scale = 1;
+
+			svg.selectAll('g#' + _.head(states) + '-' + centered[_.head(states)]).classed('hidden', false);
+			svg.selectAll(_.tail(states).map((o) => ('g.' + o + '-wrapper')).join(', ')).remove();
+			centered	= {};
 		} else {
 			x = node.width / 2;
 			y = node.height / 2;
-			k = 1;
+			scale = 1;
 
-			centered[states[curr_state]]	= null;
-			curr_state--;
+			svg.select('g#' + states[curr_state + 1] + '-' + centered[states[curr_state + 1]]).classed('hidden', false);
+			svg.selectAll('g.' + states[curr_state + 2] + '-wrapper').remove();
+			centered[states[curr_state + 1]]	= null;
 		}
 
 		let duration	= 750;
 
 		svg.transition()
 			.duration(duration)
-			.attr('transform', 'translate(' + node.width / 2 + ',' + node.height / 2 + ')scale(' + k + ')translate(' + -x + ',' + -y + ')');
+			.attr('transform', 'translate(' + node.width / 2 + ',' + node.height / 2 + ')scale(' + scale + ')translate(' + -x + ',' + -y + ')');
 
 		d3.selectAll('svg#' + map_id + ' path').transition()
 			.duration(duration)
 			.style('stroke-opacity', base_opac)
-			.style('stroke-width', (base_stroke - (curr_state * .1)) + 'px');
+			.style('stroke-width', (base_stroke - ((curr_state + 1) * .1)) + 'px');
 
 		d3.selectAll('svg#' + map_id + ' text').transition()
 			.duration(duration)
-			.style('font-size', (base_font / k) + 'px');
+			.style('font-size', (base_font / scale) + 'px');
 
 
 	}
 };
+
+function drawMap(id, state) {
+	let svg	= d3.select("svg#" + map_id + '> g');
+
+	d3.json('json/' + id + '.json', (err, raw) => {
+		let topo		= topojson.feature(raw, raw.objects.map);
+		mappedGeo[state]	= _.chain(topo).get('features', []).keyBy('properties.id').mapValues((o) => ({ centroid: path.centroid(o), bounds: path.bounds(o) })).value();
+
+		let grouped	= svg.append('g').attr('id', 'wrapped-' + id).attr('class', state + '-wrapper')
+			.selectAll('path.' + state).data(topo.features).enter().append('g')
+				.attr('id', (o) => (state + '-' + o.properties.id))
+				.attr('class', 'grouped-' + state + ' cursor-pointer');
+
+		grouped.append('path')
+			.attr('d', path)
+			.attr('class', state)
+			.attr('vector-effect', 'non-scaling-stroke')
+			.style('stroke-opacity', base_opac)
+			.style('stroke-width', (base_stroke - ((curr_state + 1) * .1)) + 'px');
+
+		grouped.append('text')
+			.attr('x', (o) => (mappedGeo[state][o.properties.id].centroid[0]))
+			.attr('y', (o) => (mappedGeo[state][o.properties.id].centroid[1]))
+			.style('font-size', (base_font / scale) + 'px')
+			.text((o) => (o.properties.name));
+
+		grouped.on('click', (o) => zoom(o.properties.id, state));
+	});
+}
