9c8ff640d18a570ad6de9a0d17879843cf448cf0
Mahdan Ahmad Fauzi Al-Hasan <mahdan.hasan@un.or.id>
2018-09-05 09:54:42 +0700

    arrow

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 moment.locale('id');
 
 $( document ).ready(function() {
+	jQuery( '#drops-wrapper img' ).each(function() {
+		let $img = jQuery(this);
+		let imgID = $img.attr('id');
+		let imgClass = $img.attr('class');
+		let imgURL = $img.attr('src');
+
+		jQuery.get(imgURL, (data) => {
+			// Get the SVG tag, ignore the rest
+			let $svg = jQuery(data).find('svg');
+
+			// Add replaced image's ID to the new SVG
+			if(typeof imgID !== 'undefined') { $svg = $svg.attr('id', imgID); }
+
+			// Add replaced image's classes to the new SVG
+			if(typeof imgClass !== 'undefined') { $svg = $svg.attr('class', imgClass.replace('hidden', '') + ' replaced-svg'); }
+
+			// Remove any invalid XML tags as per http://validator.w3.org
+			$svg = $svg.removeAttr('xmlns:a');
+
+			// Replace image with new SVG
+			$img.replaceWith($svg);
+
+		}, 'xml');
+	});
+
 	initMap();
+
 });
