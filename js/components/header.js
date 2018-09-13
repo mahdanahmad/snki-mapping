2db0920615869946693a968b4a4e06bfb732c6ed
Mahdan Ahmad Fauzi Al-Hasan <mahdan.hasan@un.or.id>
2018-09-13 17:12:05 +0700

    2 function became 1

--- js/components/header.js
+++ js/components/header.js
@@ -27,41 +27,47 @@ function changeRegionHead() {
 }
 
 function changeFilterHead(callback) {
-	let active	= $( base_target + ' > ul > li > input:checked' ).attr('value');
+	let promise	= new Promise((resolve, reject) => {
+		let active	= $( base_target + ' > ul > li > input:checked' ).attr('value');
 
-	switch (active) {
-		case layers[0]: getTypes((err, data) => { createFilterHead(data); callback(); }); break;
-		case layers[1]: createFilterHead(['2G', '3G', '4G']); callback(); break;
-		default: console.log('unhandled filter');
-	}
-}
-
-function createFilterHead(data) {
-	$( filter_target + ' > ul' ).html("<li class='toggler'>" + filt_toggle[1] + "</li>" + data.map((o, i) => (
-		"<li id='filter-" + i + "'>" +
-			"<input type='checkbox' value='" + o + "' checked>" +
-			"<label>" +
-				"<svg width='18px' height='18px' viewBox='0 0 18 18'>" +
-					"<path d='M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z'></path>" +
-					"<polyline points='1 9 7 14 15 4'></polyline>" +
-				"</svg>" +
-			"</label>" +
-			"<span>" + o + "</span>" +
-		"</li>"
-	)).join(''));
-	$( filter_target + ' > ul > li:not(.toggler)' ).click(function(e) {
-		$( this ).find('input').prop('checked', !$( this ).find('input').prop('checked'));
-
-		clearTimeout(filter_time);
-		filter_time	= setTimeout(() => { refreshView(); }, awaitTime);
-
-		$(filter_target + ' > ul > li.toggler').html(filt_toggle[($( filter_target + ' > ul > li > input:checked' ).length <= $( filter_target + ' > ul > li:not(.toggler)' ).length) ? 0 : 1]);
+		switch (active) {
+			case layers[0]: getTypes((err, data) => { resolve(data); }); break;
+			case layers[1]: resolve(['2G', '3G', '4G']); break;
+			default: reject('unhandled filter');
+		}
 	});
-	$( filter_target + ' > ul > li.toggler' ).click(function(e) {
-		$( filter_target + ' > ul > li > input' ).prop('checked', !filt_toggle.indexOf($(this).html()));
-		$(this).html(filt_toggle[(filt_toggle.indexOf($(this).html()) + 1) % 2])
-		refreshView();
-	});
+
+	promise
+		.then((data) => {
+			$( filter_target + ' > ul' ).html("<li class='toggler'>" + filt_toggle[1] + "</li>" + data.map((o, i) => (
+				"<li id='filter-" + i + "'>" +
+					"<input type='checkbox' value='" + o + "' checked>" +
+					"<label>" +
+						"<svg width='18px' height='18px' viewBox='0 0 18 18'>" +
+							"<path d='M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z'></path>" +
+							"<polyline points='1 9 7 14 15 4'></polyline>" +
+						"</svg>" +
+					"</label>" +
+					"<span>" + o + "</span>" +
+				"</li>"
+			)).join(''));
+			$( filter_target + ' > ul > li:not(.toggler)' ).click(function(e) {
+				$( this ).find('input').prop('checked', !$( this ).find('input').prop('checked'));
+
+				clearTimeout(filter_time);
+				filter_time	= setTimeout(() => { refreshView(); }, awaitTime);
+
+				$(filter_target + ' > ul > li.toggler').html(filt_toggle[($( filter_target + ' > ul > li > input:checked' ).length <= $( filter_target + ' > ul > li:not(.toggler)' ).length) ? 0 : 1]);
+			});
+			$( filter_target + ' > ul > li.toggler' ).click(function(e) {
+				$( filter_target + ' > ul > li > input' ).prop('checked', !filt_toggle.indexOf($(this).html()));
+				$(this).html(filt_toggle[(filt_toggle.indexOf($(this).html()) + 1) % 2])
+				refreshView();
+			});
+
+			callback();
+		})
+		.catch((err) => { console.error(err); });
 }
 
 function createBaseHead() {
