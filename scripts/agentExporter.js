788dbfeaf39b5cdfc3ca36e472af976faa5e5d39
Mahdan Ahmad Fauzi Al-Hasan <mahdan.hasan@un.or.id>
2018-10-03 11:13:48 +0700

    init new data

--- scripts/agentExporter.js
+++ scripts/agentExporter.js
@@ -7,7 +7,7 @@ const async			= require('async');
 const XLSX 			= require('xlsx');
 const MongoDB		= require('mongodb');
 
-const xlsx_file		= 'data/xlsx/Master_Access_Points_Pilot_V02.xlsx';
+const xlsx_file		= 'data/xlsx/Access_points_Master_Update_Pilot_V02.xlsx';
 
 const db_coll		= 'agents';
 const type_coll		= 'types';
@@ -17,6 +17,38 @@ const ObjectID		= MongoDB.ObjectID;
 
 const auth			= (process.env.DB_USERNAME !== '' || process.env.DB_PASSWORD !== '') ? process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@' : '';
 const db_url		= 'mongodb://' + auth + process.env.DB_HOST + ':' + process.env.DB_PORT;
+
+const bank_cate		= {
+	'BPD'	: 'BPD',
+	'BPRS'	: 'BPR Syariah',
+	'JV'	: 'Joint Venture Bank',
+	'PNB'	: 'Private National Bank',
+	'SB'	: 'State Bank',
+}
+
+const type_details	= {
+	'State Bank'			: { color: '#33489e', shape: 'rect', group: 'FAP' },
+	'BPD'					: { color: '#6abd45', shape: 'rect', group: 'FAP' },
+	'Private National Bank'	: { color: '#0581c3', shape: 'rect', group: 'FAP' },
+	'Joint Venture Bank'	: { color: '#70cbd3', shape: 'rect', group: 'FAP' },
+	'BPR Syariah'			: { color: '#0d8341', shape: 'rect', group: 'FAP' },
+	'ATM'					: { color: '#efd724', shape: 'circle', group: 'FAP' },
+	'Laku Pandai'			: { color: '#a64e9d', shape: 'circle', group: 'FAP' },
+	'Cooperative'			: { color: '#9b401c', shape: 'circle', group: 'FAP' },
+	'AirTime'				: { color: 'firebrick', shape: 'triangle', group: 'PAP' },
+	'Post Office'			: { color: '#f38b20', shape: 'triangle', group: 'PAP' },
+}
+
+function setType(o) {
+	if (o.bank == 'BANK') {
+		if (_.includes(['Laku Pandai', 'Laku Pandai / LKD'], o.access_point)) { return 'Laku Pandai'; }
+		else if (_.includes(['ATM'], o.access_point)) { return o.access_point; }
+		else { return bank_cate[o.bank_category]; }
+	} else {
+		if (_.includes(['Kantor Pos', 'Pos Agen'], o.access_point)) { return 'Post Office'; }
+		else { return o.access_point; }
+	}
+}
 
 MongoClient.connect(db_url, { }, (err, client) => {
 	if (err) throw err;
@@ -43,8 +75,18 @@ MongoClient.connect(db_url, { }, (err, client) => {
     			headers.push( _.snakeCase(cell.v));
 			}
 
-			let data		= XLSX.utils.sheet_to_json(sheet, { header: headers, range: 1 }).map((o) => _.mapValues(o, (m) => (m == 'NA' ? null : m)));
-			let types		= _.chain(data).map('access_point_type').uniq().map((type) => ({ type, color: null })).value();
+			let data		= XLSX.utils.sheet_to_json(sheet, { header: headers, range: 1 }).map((o) => _.chain(o).mapValues((m) => (m == 'NA' ? null : m)).assign({
+				'2_g'				: o['2_g'] ? (_.includes(o['2_g'], 'YES') ? 1 : 0) : null,
+				'3_g'				: o['3_g'] ? (_.includes(o['3_g'], 'YES') ? 1 : 0) : null,
+				'4_g'				: o['4_g'] ? (_.includes(o['4_g'], 'YES') ? 1 : 0) : null,
+				'province_id'		: o.prov_bps,
+				'kabupaten_id'		: o.kab_bps,
+				'kecamatan_id'		: o.kec_bps,
+				'desa_id'			: o.desa_bps,
+				access_point_type	: setType(o),
+			}).value());
+			// let groupMapped	= _.chain(data).uniqBy('access_point_type').map((o) => ([o.access_point_type, o.point_type])).fromPairs().value();
+			let types		= _.chain(data).map('access_point_type').uniq().map((type) => _.assign({ type }, type_details[type] )).value();
 
 			db.collection(type_coll).insertMany(types, (err, result) => {
 				let inserted	= _.chain(result.ops).map((o) => ([o.type, o._id.toString()])).fromPairs().value();
