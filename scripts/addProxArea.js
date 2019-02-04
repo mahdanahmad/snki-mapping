9d15b4bd5820dce40e930e9e6a38244c3b192fc2
Mahdan Ahmad Fauzi Al-Hasan <mahdan.hasan@un.or.id>
2019-02-04 14:26:23 +0700

    fix init

--- scripts/addProxArea.js
+++ scripts/addProxArea.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 const _				= require('lodash');
 const fs			= require('fs');
 const csv 			= require('fast-csv');
+const path			= require('path');
 const async			= require('async');
 const MongoDB		= require('mongodb');
 
-const csv_file		= 'data/csv/prox_DIY.csv';
 const DB_COLL		= 'location';
 
 const MongoClient	= MongoDB.MongoClient;
@@ -15,6 +15,10 @@ const ObjectID		= MongoDB.ObjectID;
 const auth			= (process.env.DB_USERNAME !== '' || process.env.DB_PASSWORD !== '') ? process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@' : '';
 const db_url		= 'mongodb://' + auth + process.env.DB_HOST + ':' + process.env.DB_PORT;
 
+if (!process.argv[2]) { throw 'please submit file to be inserted'; }
+
+let target			= path.join(process.cwd(), process.argv[2]);
+if (!fs.existsSync(target)) { throw 'file not found'; }
 MongoClient.connect(db_url, { }, (err, client) => {
 	if (err) throw err;
 	let db	= client.db(process.env.DB_DATABASE);
@@ -22,7 +26,7 @@ MongoClient.connect(db_url, { }, (err, client) => {
 	async.waterfall([
 		(flowCallback) => {
 			csv
-				.fromPath(csv_file, { headers: true, delimiter: ';' })
+				.fromPath(target, { headers: true, delimiter: ';' })
 				.on('data', (row) => {
 					db.collection(DB_COLL).update({ id: row.id }, { '$set': {
 						'0_5' : parseFloat(row['%Area_0to5 min'].replace(',', '.')),
