ec31b9ea438126ebbc4daf7d4d7e014838974d28
Mahdan Ahmad Fauzi Al-Hasan <mahdan.hasan@un.or.id>
2019-03-27 12:59:10 +0700

    first no select

--- js/components/header.js
+++ js/components/header.js
@@ -28,7 +28,8 @@ function createFilterHead() {
 	getTypes((err, data) => {
 		$( filter_target + ' > ul' ).html("<li class='toggler'>" + filt_toggle[1] + "</li>" + _.chain(data).groupBy('group').flatMap((val, key) => (_.concat([{ type: key, group: 'group' }], val))).map((o, i) => (
 			"<li id='filter-" + _.camelCase(o.type) + "' class='" + o.group + "'>" +
-				"<input type='checkbox' value='" + o.type + "' checked>" +
+				// "<input type='checkbox' value='" + o.type + "' checked>" +
+				"<input type='checkbox' value='" + o.type + "'>" +
 				"<label>" +
 					"<svg width='18px' height='18px' viewBox='0 0 18 18'>" +
 						"<path d='M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z'></path>" +
