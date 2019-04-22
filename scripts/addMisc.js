b3ccc46d91ac6d1dcea6bb3b6374d5b4f189e09c
Mahdan Ahmad Fauzi Al-Hasan <mahdan.hasan@un.or.id>
2019-04-22 07:31:40 +0700

    missing out

--- scripts/addMisc.js
+++ scripts/addMisc.js
@@ -31,28 +31,40 @@ MongoClient.connect(db_url, { }, (err, client) => {
 				csv
 					.fromPath(filepath, { headers: true, delimiter: ';' })
 					.on('data', (row) => {
-						data.push(row[ID_COLL]);
-						// if (row['BPS_ID#'] !== "") {
-						// 	// console.log(_.chain(row).omit([ID_COLL]).omitBy((o, key) => (_.includes(key, 'NAME'))).mapValues(o => (_.includes(['NA', ''], o) ? null : o )).value());
-						// 	db.collection(DB_COLL).updateOne({ id: row[ID_COLL] }, { '$set': _.chain(row).omit([ID_COLL]).omitBy((o, key) => (_.includes(key, 'NAME'))).mapValues(o => (_.includes(['NA', ''], o) ? null : o )).value() }, {}, (err) => { if (err) { next(err) } })
-						// }
+						// data.push(row[ID_COLL]);
+						if (row['BPS_ID#'] !== "") {
+							// console.log(_.chain(row).omit([ID_COLL]).omitBy((o, key) => (_.includes(key, 'NAME'))).mapValues(o => (_.includes(['NA', ''], o) ? null : o )).value());
+							db.collection(DB_COLL).updateOne({ id: row[ID_COLL] }, { '$set': _.chain(row).omit([ID_COLL]).omitBy((o, key) => (_.includes(key, 'NAME'))).mapValues(o => (_.includes(['NA', ''], o) ? null : o )).value() }, {}, (err) => { if (err) { next(err) } })
+						}
 					})
 					.on('finish', () => { next(null, data); })
 			}, flowCallback);
 		},
-		(inputs, flowCallback) => {
-			inputs = _.flatten(inputs);
-			db.collection(DB_COLL).find({type: { '$ne': 'desa' }}).project({ id: 1, name: 1, _id: 0 }).toArray().then(result => {
-				let ids 	= _.map(result, 'id');
-				let nots	= _.difference(ids, inputs);
-
-				console.log(not);
-
-				// csv
-				// 	.writeToPath('./results/kecamatan.csv', _.chain(result).filter(o => (_.includes(nots, o.id))).value(), { headers: true })
-				// 	.on('finish', () => { flowCallback(); })
-			});
-		}
+		// (inputs, flowCallback) => {
+		// 	inputs = _.flatten(inputs);
+		// 	db.collection(DB_COLL).find({type: { '$ne': 'desa' }}).project({ id: 1, name: 1, parent: 1, _id: 0 }).toArray().then(result => {
+		// 		let ids 	= _.map(result, 'id');
+		// 		let nots	= _.difference(ids, inputs);
+		// 		let missing	= _.chain(result).filter(o => (_.includes(nots, o.id))).value();
+		//
+		// 		let kabs	= _.chain(missing).map('parent').uniq().value();
+		// 		let det_kab	= _.chain(result).filter(o => (_.includes(kabs, o.id))).value();
+		//
+		// 		let provs	= _.chain(det_kab).map('parent').uniq().value();
+		// 		let det_pro	= _.chain(result).filter(o => (_.includes(provs, o.id))).value();
+		// 		let map_pro	= _.chain(det_pro).map(o => ({ prov_id: o.id, prov_name: o.name })).keyBy('prov_id').value();
+		//
+		// 		let map_kab	= _.chain(det_kab).map(o => _.assign({ kab_id: o.id, kab_name: o.name }, map_pro[o.parent])).keyBy('kab_id').value();
+		//
+		// 		let concated	= _.chain(missing).map(o => _.assign({}, _.omit(o, ['parent']), map_kab[o.parent])).value()
+		//
+		// 		// console.log(concated);
+		//
+		// 		csv
+		// 			.writeToPath('./results/missing_kecamatan.csv', concated, { headers: true })
+		// 			.on('finish', () => { flowCallback(); })
+		// 	});
+		// }
 	], (err) => {
 		if (err) throw err;
 
