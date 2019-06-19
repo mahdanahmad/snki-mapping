e3639e072168d73e01a0a56bdfe632c6f9f8d433
Mahdan Ahmad Fauzi Al Hasan <mahdan.ahmad@yahoo.com>
2019-06-20 01:07:37 +0700

    change filter

--- scripts/agentExporter.js
+++ scripts/agentExporter.js
@@ -6,8 +6,8 @@ const csv 			= require('fast-csv');
 const async			= require('async');
 const MongoDB		= require('mongodb');
 
-const bank_file		= 'data/init/banklist.csv';
-const csv_file		= 'data/init/asuransi.csv';
+const bank_file		= 'data/banklist.csv';
+const csv_file		= 'data/results.csv';
 
 const db_coll		= 'agents';
 const type_coll		= 'types';
@@ -35,19 +35,19 @@ MongoClient.connect(db_url, { useNewUrlParser: true }, (err, client) => {
 		// 	db.collection(db_coll).deleteMany({}, (err) => flowCallback(err));
 		// },
 		(flowCallback) => {
-			db.collection(type_coll).findOne({ 'type': 'Insurance' }, (err, result) => {
+			db.collection(type_coll).findOne({ 'type': 'LKM' }, (err, result) => {
 				if (err) { flowCallback(err); }
 				else if (result) { flowCallback(); } else {
-					let asuransi = {
-						"type"	: "Insurance",
-						"color"	: "#3A606E",
+					let newType = {
+						"type"	: "LKM",
+						"color"	: "firebrick",
 						"shape"	: "circle",
-						"group"	: "FAP",
-						"en"	: "Insurance",
-						"id"	: "Asuransi"
+						"group"	: "PAP",
+						"en"	: "LKM",
+						"id"	: "LKM"
 					}
 
-					db.collection(type_coll).insertOne(asuransi, (err) => flowCallback(err));
+					db.collection(type_coll).insertOne(newType, (err) => flowCallback(err));
 				}
 			});
 		},
@@ -66,15 +66,27 @@ MongoClient.connect(db_url, { useNewUrlParser: true }, (err, client) => {
 			let data	= [];
 			let typesMapped	= _.chain(types).map(o => ([o.type, o._id])).fromPairs().value()
 
+			let prov	= {};
+
 			csv
 				.fromPath(csv_file, { headers: true })
 				.on('data', (row) => {
-					let access_point_type	= _.includes(_.keys(bank_cate), row.access_point_type) ? bank_cate[bankmapped[row.bank_id]] : row.access_point_type;
+					let access_point_type = '';
+					if (row['Institutio'] == 'INSURANSI') { access_point_type = '' }
+					else if (row['ACCESS POI'] == 'Pergadaian') { access_point_type = 'Pegadaian' }
+					else if (_.includes(['ATM', 'LKM'], row['ACCESS POI'])) { access_point_type = row['ACCESS POI'] }
+					else {
+						if (!row['BANKID']) { console.log(row); } else { access_point_type = bank_cate[bankmapped[parseInt(row['BANKID'])]] }
+					}
 
-					data.push(_.chain(row).omit(['bank_name', 'bank_id']).assign({ desa_id: parseInt(row.desa_id).toString(), access_point_type, access_point_id: typesMapped[access_point_type] }).value())
+					if (access_point_type) {
+						data.push(_.chain(row).omit(['bank_name', 'bank_id']).assign({ desa_id: parseInt(row.desa_id).toString(), access_point_type, access_point_id: typesMapped[access_point_type] }).value())
+					}
+
+					if (!_.chain(prov).keys().includes(row['province_id']).value()) { prov[row['province_id']] = 1; } else { prov[row['province_id']]++; }
 				})
-				.on('finish', () => { db.collection(db_coll).insertMany(data, (err) => flowCallback(err)); })
-				// .on('finish', () => { flowCallback() })
+				.on('finish', () => { console.log(data.length); db.collection(db_coll).insertMany(data, (err) => flowCallback(err)); })
+				// .on('finish', () => { console.log(prov); console.log(_.chain(prov).values().sum().value()); flowCallback() })
 		}
 	], (err) => {
 		if (err) throw err;
